79ed4c5ede918968ba6ed04201d4190cab75f0f0
Jerome Young <dreammedia365@yahoo.com>
2020-08-20 09:56:45 -0700

    Updates

---
+++ precache-manifest.b992851f1b8efcb25262fb4d476ba638.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b34f994c8f697f4484b803aa5c86e34",
+    "url": "/amazon-project/index.html"
+  },
+  {
+    "revision": "fd693bac2ab442cce26a",
+    "url": "/amazon-project/static/css/main.7bfb2f8d.chunk.css"
+  },
+  {
+    "revision": "3acdb46c94d63f716bc3",
+    "url": "/amazon-project/static/js/2.c206f814.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/amazon-project/static/js/2.c206f814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd693bac2ab442cce26a",
+    "url": "/amazon-project/static/js/main.f46572ab.chunk.js"
+  },
+  {
+    "revision": "08ea6d200bef48f56250",
+    "url": "/amazon-project/static/js/runtime-main.08cfde91.js"
+  }
+]);
